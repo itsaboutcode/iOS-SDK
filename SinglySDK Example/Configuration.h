c8d290784627449bfac5c9864336e7ee107b9055
Justin Mecham <justin@mecham.me>
2012-10-12 15:52:37 -0400

    Added a SinglyConfiguration.h file for defining client-specific keys.

---
+++ SinglySDK Example/Configuration.h
@@ -0,0 +1,3 @@
+
+#define TEST_FOO @"BOOOO"
+
---
+++ SinglySDK Example/SinglySDK Example/SinglyConfiguration.h
@@ -0,0 +1,39 @@
+//
+//  SinglyConfiguration.h
+//  SinglySDK Example
+//
+//
+//  This file contains sensitive information that should not be readily shared,
+//  especially if you are pushing this to a public repository. Before you edit
+//  this file, you should run:
+//
+//    git update-index --assume-unchanged SinglyConfiguration.h
+//
+//  This will ignore any changes you make to this file.
+//
+
+/*!
+ *
+ * Your Singly Client ID and Secret. If you don't already have these, you may
+ * obtain them by signing up for a Singly account at https://singly.com/signup.
+ *
+ */
+#define CLIENT_ID @""
+#define CLIENT_SECRET @""
+
+/*!
+ *
+ * Your Singly Client ID and Secret. If you don't already have these, you may
+ * obtain them by signing up for a Singly account at https://singly.com/signup.
+ *
+ */
+#define CLIENT_ID @""
+#define CLIENT_SECRET @""
+
+/*!
+ *
+ * TestFlight token for distribution (optional). Learn more about TestFlight at
+ * http://www.testflight.com
+ *
+ */
+// #define TESTFLIGHT_TOKEN @""
